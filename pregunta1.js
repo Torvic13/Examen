fd2227800be523a34b91ac3650716beaaa08fabe
Torvic13 <torvic123.vcc45@gmail.com>
2024-09-11 21:49:24 -0500

    Add files via upload

---
+++ pregunta1.js
@@ -0,0 +1,5 @@
+function nombre(){
+    document.getElementById("fapellido").innerText = "calllahui";
+    document.getElementById("fname").innerText = "VICTOR"
+    document.getElementById("faño").innerText = "24"
+}
---
+++ pregunta2.js
@@ -0,0 +1,6 @@
+function imc(){
+    let altura = document.getAnimations("faltura");
+    let peso = document.getElementById("fpeso");
+    document.getElementById("demo").innerHTML = altura + peso;
+
+}
